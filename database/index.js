f9ed6827201c15c6d9a409abc148c8a4514be678
Luke Hatcher <lukehatcher98@gmail.com>
2020-12-26 01:02:52 -0800

    working project and todo creating functions

--- database/index.js
+++ database/index.js
@@ -62,39 +62,53 @@ const deleteProject = async (user, project) => {
   await doc.save();
 };
 
-const addProject = async (data) => {
-  // based off username... find that users doc
-  // add project
+const addProject = async (user, newProject) => {
+  const doc = await UserInfo.findOne({ username: user });
+  const emptyProj = {
+    projectName: newProject,
+    todos: [],
+  };
+  doc.projects.push(emptyProj);
+  await doc.save();
 };
 
-const addTodo = async (data) => {
-  // based of username and project name...
-  // push that todo onto the end of that projects todo array
+const addTodo = async (user, project, newTodo) => {
+  const doc = await UserInfo.findOne({ username: user });
+  const projectIdx = doc.projects
+    .findIndex((item) => item.projectName === project);
+  doc.projects[projectIdx].todos.push(newTodo);
+  await doc.save();
 };
 
 // example data for testing database functions
-const exData = {
-  username: 'jon doe',
-  projects: [{
-    projectName: 'app1',
-    todos: ['fix errors', 'check linter', 'install packages', 'do things', 'run', 'up', 'a', 'b', 'c', 'd', 'e', 'efefe', 'sssss', 'sdfsfdsfs', 'dfdfdfdfdfn',
-    ],
-  },
-  {
-    projectName: 'app2',
-    todos: ['fix more errors', 'configure files', 'delete comments', 'fix bug on line 55'],
-  }],
-};
+// ========================================
+// const exData = {
+//   username: 'jon doe',
+//   projects: [{
+//     projectName: 'app1',
+//     todos: ['fix errors', 'check linter', 'install packages', 'do things', 'run', 'up', 'a', 'b', 'c', 'd', 'e', 'efefe', 'sssss', 'sdfsfdsfs', 'dfdfdfdfdfn',
+//     ],
+//   },
+//   {
+//     projectName: 'app2',
+//     todos: ['fix more errors', 'configure files', 'delete comments', 'fix bug on line 55'],
+//   }],
+// };
 
 // example calls to test database functions
+// ========================================
 // initUserdata(exData);
 // getUserData('jon doe');
 // deleteTodo('jon doe2', 'app1', 'install packages');
 // deleteProject('jon doe', 'app2');
+// addTodo('jon doe', 'app2', 'center div');
+// addProject('jon doe', 'app3');
 
 module.exports = {
   initUserdata,
   getUserData,
   deleteTodo,
   deleteProject,
+  addProject,
+  addTodo,
 };
